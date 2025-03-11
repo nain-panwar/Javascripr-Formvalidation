b3950d35f3bb7b72f7f312f46aec98109d66ad91
nain-panwar <nainpanwar021@gmail.com>
2025-03-11 19:29:59 +0530

    java script form validation

---
+++ script.js
@@ -0,0 +1,31 @@
+document.querySelector('#form-submit').addEventListener('click', function () {
+
+    if (document.querySelector('.firstname').value === '') {
+        document.querySelector('.error').innerText = 'First Name is wrong.';
+        return;
+    } else {
+        document.querySelector('.error').innerText = '';
+    }
+
+    if (document.querySelector('.lastname').value === '') {
+        document.querySelector('.error').innerText = 'Last Name is wrong.';
+        return;
+    } else {
+        document.querySelector('.error').innerText = '';
+    }
+
+    if (document.querySelector('.phone').value === '') {
+        document.querySelector('.error').innerText = 'Phone number is wrong.';
+        return;
+    } else {
+        document.querySelector('.error').innerText = '';
+    }
+
+    if (document.querySelector('.address').value === '') {
+        document.querySelector('.error').innerText = 'Address is wrong.';
+        return;
+    } else {
+        document.querySelector('.error').innerText = '';
+    }
+
+});
